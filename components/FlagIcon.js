2e280654c7b8461581a4280c1ef69628ea2962ba
Afrie Irham <afrieirham.work@gmail.com>
2023-01-23 05:15:28 +0800

    feat: add location specific pages

---
+++ components/FlagIcon.js
@@ -0,0 +1,139 @@
+import { Img } from "@chakra-ui/react";
+import React from "react";
+
+function FlagIcon({ name }) {
+  switch (name) {
+    case "johor":
+      return (
+        <Img
+          src="https://upload.wikimedia.org/wikipedia/commons/5/5a/Flag_of_Johor.svg"
+          alt="Johor flag"
+          width="36px"
+        />
+      );
+    case "kedah":
+      return (
+        <Img
+          src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Flag_of_Kedah.svg"
+          alt="Kedah flag"
+          width="36px"
+        />
+      );
+    case "kelantan":
+      return (
+        <Img
+          src="https://upload.wikimedia.org/wikipedia/commons/6/61/Flag_of_Kelantan.svg"
+          alt="kelantan flag"
+          width="36px"
+        />
+      );
+    case "kuala-lumpur":
+      return (
+        <Img
+          src="https://upload.wikimedia.org/wikipedia/commons/6/64/Flag_of_Kuala_Lumpur%2C_Malaysia.svg"
+          alt="kuala lumpur flag"
+          width="36px"
+        />
+      );
+    case "labuan":
+      return (
+        <Img
+          src="https://upload.wikimedia.org/wikipedia/commons/0/00/Flag_of_the_Federal_Territories_of_Malaysia.svg"
+          alt="labuan flag"
+          width="36px"
+        />
+      );
+    case "melaka":
+      return (
+        <Img
+          src="https://upload.wikimedia.org/wikipedia/commons/0/09/Flag_of_Malacca.svg"
+          alt="melaka flag"
+          width="36px"
+        />
+      );
+    case "negeri-sembilan":
+      return (
+        <Img
+          src="https://upload.wikimedia.org/wikipedia/commons/d/db/Flag_of_Negeri_Sembilan.svg"
+          alt="negeri sembilan flag"
+          width="36px"
+        />
+      );
+    case "pahang":
+      return (
+        <Img
+          src="https://upload.wikimedia.org/wikipedia/commons/a/aa/Flag_of_Pahang.svg"
+          alt="pahang flag"
+          width="36px"
+        />
+      );
+    case "perak":
+      return (
+        <Img
+          src="https://upload.wikimedia.org/wikipedia/commons/8/87/Flag_of_Perak.svg"
+          alt="perak flag"
+          width="36px"
+        />
+      );
+    case "perlis":
+      return (
+        <Img
+          src="https://upload.wikimedia.org/wikipedia/commons/a/aa/Flag_of_Perlis.svg"
+          alt="perlis flag"
+          width="36px"
+        />
+      );
+    case "penang":
+    case "pulau-pinang":
+      return (
+        <Img
+          src="https://upload.wikimedia.org/wikipedia/commons/d/d4/Flag_of_Penang_%28Malaysia%29.svg"
+          alt="pulau pinang flag"
+          width="36px"
+        />
+      );
+    case "putrajaya":
+      return (
+        <Img
+          src="https://upload.wikimedia.org/wikipedia/commons/9/9f/Flag_of_Putrajaya.svg"
+          alt="putrajaya flag"
+          width="36px"
+        />
+      );
+    case "sabah":
+      return (
+        <Img
+          src="https://upload.wikimedia.org/wikipedia/commons/b/b5/Flag_of_Sabah.svg"
+          alt="sabah flag"
+          width="36px"
+        />
+      );
+    case "sarawak":
+      return (
+        <Img
+          src="https://upload.wikimedia.org/wikipedia/commons/7/7e/Flag_of_Sarawak.svg"
+          alt="sarawak flag"
+          width="36px"
+        />
+      );
+    case "selangor":
+      return (
+        <Img
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0c/Flag_of_Selangor.svg"
+          alt="selangor flag"
+          width="36px"
+        />
+      );
+    case "terengganu":
+      return (
+        <Img
+          src="https://upload.wikimedia.org/wikipedia/commons/6/6b/Flag_of_Terengganu.svg"
+          alt="terengganu flag"
+          width="36px"
+        />
+      );
+  }
+  return <div>FlagIcon</div>;
+}
+
+export default FlagIcon;
---
+++ pages/location/[place].js
@@ -0,0 +1,134 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Flex,
+  Heading,
+  HStack,
+  Text,
+} from "@chakra-ui/react";
+import Head from "next/head";
+import React from "react";
+
+import FlagIcon from "../../components/FlagIcon";
+import JobListing from "../../components/JobListing";
+import { getJobsByKeyword } from "../../controllers/jobs";
+import { capitalize } from "../../helpers/capitalize";
+
+export const getStaticProps = async (context) => {
+  const { place } = context.params;
+
+  let keywords = [place.replace("-", " ")];
+  if (place === "ns") {
+    keywords = ["negeri sembilan"];
+  }
+  if (place === "penang") {
+    keywords = ["pulau pinang"];
+  }
+  if (place === "kl") {
+    keywords = ["kuala lumpur"];
+  }
+
+  const jobs = await getJobsByKeyword(keywords);
+
+  return {
+    props: {
+      jobs,
+      place,
+    },
+  };
+};
+
+export async function getStaticPaths() {
+  const places = [
+    "johor",
+    "kedah",
+    "kelantan",
+    "melaka",
+    "negeri-sembilan",
+    "ns",
+    "pahang",
+    "perak",
+    "perlis",
+    "pulau-pinang",
+    "penang",
+    "sarawak",
+    "selangor",
+    "terengganu",
+    "kuala-lumpur",
+    "kl",
+    "labuan",
+    "sabah",
+    "putrajaya",
+  ];
+
+  const paths = places.map((item) => ({ params: { place: item } }));
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+function Framework({ jobs, place }) {
+  const getActualPlace = () => {
+    switch (place) {
+      case "ns":
+        return "negeri-sembilan";
+      case "kl":
+        return "kuala-lumpur";
+      default:
+        return place;
+    }
+  };
+
+  const actualPlace = getActualPlace();
+  const state = capitalize(actualPlace.replace("-", " "));
+  const pageTitle = `Tech Jobs in ${state}`;
+
+  return (
+    <div>
+      <Head>
+        <title>{pageTitle}</title>
+        <meta name="description" content={pageTitle} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Flex flexDirection="column" maxW="2xl" mx="auto" p="4">
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <BreadcrumbLink href={`/`}>Home</BreadcrumbLink>
+          </BreadcrumbItem>
+
+          <BreadcrumbItem isCurrentPage>
+            <BreadcrumbLink
+              href={`/location/${place}`}
+              textTransform="capitalize"
+            >
+              {state}
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        </Breadcrumb>
+
+        <HStack mt="2">
+          <Heading size="md" textTransform="capitalize">
+            {pageTitle}
+          </Heading>
+          <FlagIcon name={actualPlace} />
+        </HStack>
+
+        <Flex mt="4">
+          <Text fontSize="xs" color="gray.600">
+            Total jobs: {jobs.length}
+          </Text>
+        </Flex>
+
+        <Flex flexDirection="column">
+          {jobs.map((job) => (
+            <JobListing key={job._id} job={job} />
+          ))}
+        </Flex>
+      </Flex>
+    </div>
+  );
+}
+
+export default Framework;
