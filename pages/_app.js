5d8f02f6d8513bb9c435291446e3c04d32d81380
Afrie Irham <afrieirham.work@gmail.com>
2023-01-25 23:47:18 +0800

    feat: add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
+import { Analytics } from "@vercel/analytics/react";
 import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
+      <Analytics />
     </ChakraProvider>
   );
 }
