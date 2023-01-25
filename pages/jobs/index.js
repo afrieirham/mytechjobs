6ffcefa9a02b5c556288886a517b9c5c72cebf7d
Afrie Irham <afrieirham.work@gmail.com>
2023-01-25 23:59:57 +0800

    chore: tweak /jobs SEO stuff

--- pages/jobs/index.js
+++ pages/jobs/index.js
@@ -69,22 +69,26 @@ function Search() {
   return (
     <div>
       <Head>
-        <title>Search – Malaysia Tech Jobs 🇲🇾</title>
+        <title>Find your next tech jobs with MY Tech Jobs 🇲🇾</title>
         <meta
           name="description"
-          content={`Daily tech jobs sourced from ${sites.join(", ")}.`}
+          content={`MYtechjobs.com is a job board that compiles tech jobs from sites like ${sites.join(
+            ", "
+          )} daily.`}
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Flex flexDirection="column" maxW="2xl" mx="auto" p="4" mb="16">
         {/* Header */}
-        <Flex flexDirection="column" w="full">
-          <Heading size="md">Search Tech Jobs in Malaysia🇲🇾</Heading>
+        <Flex flexDirection="column" w="full" alignItems="center" mt="4">
+          <Heading size="lg">Find Tech Jobs in Malaysia 🇲🇾</Heading>
         </Flex>
 
         {/* Job Listing */}
-        <Flex flexDirection="column" w="full" mt="8">
-          <Heading size="sm">👨🏻‍💻 Job Listing</Heading>
+        <Flex flexDirection="column" w="full" mt="4">
+          <Heading size="sm" mt="4">
+            👨🏻‍💻 Explore Jobs
+          </Heading>
           <HStack mt="4">
             <Text fontSize="xs" color="gray.600">
               Showing results for:
