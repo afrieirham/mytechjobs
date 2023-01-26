972d380c71f32a83c7c40aa7438cafab4cd3b6a5
Afrie Irham <afrieirham.work@gmail.com>
2023-01-26 11:12:41 +0800

    chore: protect /api/hello

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,8 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { getAllSlugs } from "../../controllers/jobs";
-
 export default async function handler(req, res) {
-  const { slugs } = await getAllSlugs();
-  res.status(200).json({ name: "John Doe", slugs });
+  res.status(200).json({ name: "John Doe" });
 }
