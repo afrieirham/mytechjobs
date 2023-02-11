e77f8d702fb13b26901d90ebfcf17b4ad2ec5600
Afrie Irham <afrieirham.work@gmail.com>
2023-02-11 15:23:18 +0800

    view: add hire me button

--- pages/talents/[id].js
+++ pages/talents/[id].js
@@ -2,6 +2,7 @@ import React from "react";
 import Head from "next/head";
 import fetch from "node-fetch";
 import { Tag } from "@chakra-ui/tag";
+import { Button } from "@chakra-ui/button";
 import { Flex, Heading, Text, VStack } from "@chakra-ui/layout";
 import {
   Breadcrumb,
@@ -10,29 +11,33 @@ import {
 } from "@chakra-ui/breadcrumb";
 
 import GlobalHeader from "../../components/GlobalHeader";
+const dataUrl = "https://kerja-it-talents.vercel.app/talents";
 
 export const getStaticProps = async (context) => {
   const { id } = context.params;
-  const dataUrl = "https://kerja-it-talents.vercel.app/talents/";
-  const profile = await fetch(dataUrl + id).then((res) => res.json());
+  const profile = await fetch(`${dataUrl}/${id}`).then((res) => res.json());
 
   return {
     props: {
       profile,
     },
-    // revalidate every 1 hour
-    revalidate: 60 * 60 * 1,
+    // revalidate every 1 minute
+    revalidate: 60 * 1,
   };
 };
 
 export async function getStaticPaths() {
-  const ids = [1, 2, 3, 4, 5, 6, 7].map(String);
-  const paths = ids.map((id) => ({ params: { id } }));
+  const profile = await fetch(dataUrl).then((res) => res.json());
+  const paths = profile.map(({ id }) => ({ params: { id: String(id) } }));
 
   return {
     paths,
     fallback: false,
   };
+}
+
+function HireMeButton() {
+  return <Button w={{ base: "full", lg: "200px" }}>Hire me 💼</Button>;
 }
 
 function Profile({ profile }) {
@@ -68,20 +73,21 @@ function Profile({ profile }) {
         borderRadius={{ base: "none", md: "lg" }}
         mt="4"
       >
-        <Heading>Developer {id}</Heading>
-        <VStack alignItems="flex-start" spacing="6" mt="4">
+        <Flex w="full" justifyContent="space-between">
+          <Heading size="lg">Developer {id}</Heading>
+          {activelyLooking && (
+            <Tag size="sm" colorScheme="green">
+              Actively looking
+            </Tag>
+          )}
+        </Flex>
+        <VStack alignItems="flex-start" spacing="6" mt="6">
+          <HireMeButton />
           <Flex flexDirection="column">
             <Text fontSize="sm" fontWeight="bold">
               Available date:
             </Text>
-            <Text>
-              {profile["Available to start on"]}{" "}
-              {activelyLooking && (
-                <Tag size="sm" colorScheme="green">
-                  Actively looking
-                </Tag>
-              )}
-            </Text>
+            <Text>{profile["Available to start on"]} </Text>
           </Flex>
           <Flex flexDirection="column">
             <Text fontSize="sm" fontWeight="bold">
@@ -133,11 +139,12 @@ function Profile({ profile }) {
               fontFamily="sans-serif"
               whiteSpace="pre-wrap"
               fontSize="sm"
-              color="gray.600"
+              color="gray.700"
             >
               {profile["Bio"]}
             </Text>
           </Flex>
+          <HireMeButton />
         </VStack>
       </Flex>
     </div>
