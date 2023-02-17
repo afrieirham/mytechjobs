9d6279b17cbc5a07d4d006ce1b4fa8d120abe76a
Afrie Irham <afrieirham.work@gmail.com>
2023-02-17 10:38:06 +0800

    fix: use fallback true for /talents/[id]

--- pages/talents/[id].js
+++ pages/talents/[id].js
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import NextLink from "next/link";
 import fetch from "node-fetch";
 import { Tag } from "@chakra-ui/tag";
 import { Button } from "@chakra-ui/button";
@@ -31,7 +32,7 @@ export async function getStaticPaths() {
 
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 }
 
@@ -49,111 +50,138 @@ function HireMeButton() {
 }
 
 function Profile({ profile }) {
-  const { id } = profile;
-  const title = `Developer ${id} | Kerja IT`;
-  const activelyLooking = profile["Are you actively looking?"] === "Yes";
+  const hasProfile = Object.keys(profile ?? {}).length > 0;
+  const title = profile
+    ? `Developer ${profile.id} | Kerja IT`
+    : "Developer Not Found | Kerja IT";
+  const activelyLooking = profile?.["Are you actively looking?"] === "Yes";
 
   return (
     <div>
       <Head>
         <title>{title}</title>
       </Head>
-      <Breadcrumb mt="8" maxW="2xl" mb="4" mx={{ base: "4", md: "auto" }}>
-        <BreadcrumbItem>
-          <BreadcrumbLink href="/talents">Talents</BreadcrumbLink>
-        </BreadcrumbItem>
+      {hasProfile && (
+        <Breadcrumb mt="8" maxW="2xl" mb="4" mx={{ base: "4", md: "auto" }}>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/talents">Talents</BreadcrumbLink>
+          </BreadcrumbItem>
 
-        <BreadcrumbItem isCurrentPage>
-          <BreadcrumbLink href={`/talents/${id}`}>
-            Developer {id}
-          </BreadcrumbLink>
-        </BreadcrumbItem>
-      </Breadcrumb>
-      <Flex
-        flexDirection="column"
-        maxW="2xl"
-        mx="auto"
-        p="4"
-        bg="white"
-        borderColor="gray.300"
-        borderWidth={{ base: "none", md: "1px" }}
-        borderRadius={{ base: "none", md: "lg" }}
-        mt="4"
-      >
-        <Flex w="full" justifyContent="space-between">
-          <Heading size="lg">Developer {id}</Heading>
-          {activelyLooking && (
-            <Tag size="sm" colorScheme="green">
-              Actively looking
-            </Tag>
-          )}
+          <BreadcrumbItem isCurrentPage>
+            <BreadcrumbLink href={`/talents/${profile.id}`}>
+              Developer {profile.id}
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        </Breadcrumb>
+      )}
+      {!hasProfile && (
+        <Flex
+          flexDirection="column"
+          maxW="2xl"
+          mx="auto"
+          p={{ base: "4", md: "0" }}
+          textAlign="center"
+          alignItems="center"
+          mt="8"
+        >
+          <Heading size="md">😵‍💫 Opps...</Heading>
+          <Heading size="sm" mt="2" color="gray.600">
+            Can&apos;t find the candidate you&apos;re looking for.
+          </Heading>
+          <VStack mt="8">
+            <Button href="/talents" as={NextLink}>
+              Find other candidates 🔎
+            </Button>
+          </VStack>
         </Flex>
-        <VStack alignItems="flex-start" spacing="6" mt="6">
-          <HireMeButton />
-          <Flex flexDirection="column">
-            <Text fontSize="sm" fontWeight="bold">
-              Available date:
-            </Text>
-            <Text>{profile["Available to start on"]} </Text>
+      )}
+      {hasProfile && (
+        <Flex
+          flexDirection="column"
+          maxW="2xl"
+          mx="auto"
+          p="4"
+          bg="white"
+          borderColor="gray.300"
+          borderWidth={{ base: "none", md: "1px" }}
+          borderRadius={{ base: "none", md: "lg" }}
+          mt="4"
+        >
+          <Flex w="full" justifyContent="space-between">
+            <Heading size="lg">Developer {profile.id}</Heading>
+            {activelyLooking && (
+              <Tag size="sm" colorScheme="green">
+                Actively looking
+              </Tag>
+            )}
           </Flex>
-          <Flex flexDirection="column">
-            <Text fontSize="sm" fontWeight="bold">
-              Available in:
-            </Text>
-            <Text>{profile["Work location"]}</Text>
-          </Flex>
-          <Flex flexDirection="column">
-            <Text fontSize="sm" fontWeight="bold">
-              Looking for:
-            </Text>
-            <Text>{profile["Role Type"]}</Text>
-          </Flex>
-          <Flex flexDirection="column">
-            <Text fontSize="sm" fontWeight="bold">
-              Preferred position:
-            </Text>
-            <Text>{profile["Preferred Role"]}</Text>
-          </Flex>
-          <Flex flexDirection="column">
-            <Text fontSize="sm" fontWeight="bold">
-              Interested in role as:
-            </Text>
-            <Text>{profile["Role Level"]}</Text>
-          </Flex>
-          <Flex flexDirection="column">
-            <Text fontSize="sm" fontWeight="bold">
-              Remote:
-            </Text>
-            <Text>{profile["Are you open to remote job?"]}</Text>
-          </Flex>
-          <Flex flexDirection="column">
-            <Text fontSize="sm" fontWeight="bold">
-              On-site:
-            </Text>
-            <Text>{profile["Are you open to on-site job?"]}</Text>
-          </Flex>
-          <Flex flexDirection="column">
-            <Text fontSize="sm" fontWeight="bold">
-              Skills:
-            </Text>
-            <Text>{profile["List down your skills "][" tech stacks"]}</Text>
-          </Flex>
-          <Flex flexDirection="column">
-            <Text fontSize="sm" fontWeight="bold">
-              Profile:
-            </Text>
-            <Text
-              fontFamily="sans-serif"
-              whiteSpace="pre-wrap"
-              fontSize="sm"
-              color="gray.700"
-            >
-              {profile["Bio"]}
-            </Text>
-          </Flex>
-          <HireMeButton />
-        </VStack>
-      </Flex>
+          <VStack alignItems="flex-start" spacing="6" mt="6">
+            <HireMeButton />
+            <Flex flexDirection="column">
+              <Text fontSize="sm" fontWeight="bold">
+                Available date:
+              </Text>
+              <Text>{profile["Available to start on"]} </Text>
+            </Flex>
+            <Flex flexDirection="column">
+              <Text fontSize="sm" fontWeight="bold">
+                Available in:
+              </Text>
+              <Text>{profile["Work location"]}</Text>
+            </Flex>
+            <Flex flexDirection="column">
+              <Text fontSize="sm" fontWeight="bold">
+                Looking for:
+              </Text>
+              <Text>{profile["Role Type"]}</Text>
+            </Flex>
+            <Flex flexDirection="column">
+              <Text fontSize="sm" fontWeight="bold">
+                Preferred position:
+              </Text>
+              <Text>{profile["Preferred Role"]}</Text>
+            </Flex>
+            <Flex flexDirection="column">
+              <Text fontSize="sm" fontWeight="bold">
+                Interested in role as:
+              </Text>
+              <Text>{profile["Role Level"]}</Text>
+            </Flex>
+            <Flex flexDirection="column">
+              <Text fontSize="sm" fontWeight="bold">
+                Remote:
+              </Text>
+              <Text>{profile["Are you open to remote job?"]}</Text>
+            </Flex>
+            <Flex flexDirection="column">
+              <Text fontSize="sm" fontWeight="bold">
+                On-site:
+              </Text>
+              <Text>{profile["Are you open to on-site job?"]}</Text>
+            </Flex>
+            <Flex flexDirection="column">
+              <Text fontSize="sm" fontWeight="bold">
+                Skills:
+              </Text>
+              <Text>{profile["List down your skills "][" tech stacks"]}</Text>
+            </Flex>
+            <Flex flexDirection="column">
+              <Text fontSize="sm" fontWeight="bold">
+                Profile:
+              </Text>
+              <Text
+                fontFamily="sans-serif"
+                whiteSpace="pre-wrap"
+                fontSize="sm"
+                color="gray.700"
+              >
+                {profile["Bio"]}
+              </Text>
+            </Flex>
+            <HireMeButton />
+          </VStack>
+        </Flex>
+      )}
       <HStack maxW="2xl" mx="auto" p="8" justifyContent="center">
         <Link href="/connect" isExternal textAlign="center">
           You&apos;re a developer? Add your profile ✍️
